f622a47a82468cbf18b24e80730b0488726098b3
Inok94 <rodiknikolaev@yandex.ru>
2019-11-15 10:26:39 +0300

    (feature) Add migrations

--- database/migrations/2019_11_14_193959_create_schedules_table.php
+++ database/migrations/2019_11_14_193959_create_schedules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSchedulesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('schedules', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('user_type_id')->nullable();
-            $table->rememberToken();
+            $table->integer('doctor_id');
+            $table->integer('user_id');
+            $table->integer('service_id');
+            $table->dateTime('data_patient_visit');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('schedules');
     }
 }
